9c5395721fe8a189855b634559ba4d24f177c9d7
Ashfaq4Ahmed <100022026+Ashfaq4Ahmed@users.noreply.github.com>
2022-05-10 10:51:02 +0500

    chapter 26-30

---
+++ chapters26-30.js
@@ -0,0 +1,151 @@
+
+
+// Q1. 
+
+
+// Write a program that takes a positive integer from user & display the following in your browser.
+//  a. number
+//   b. round off value of the number 
+//   c. floor value of the number
+    // d. ceil value of the number
+
+
+
+    // var num =  +prompt("type any number please","Enter here");
+
+    // var roundnum = Math.round(num);
+    // var floornum = Math.floor(num);
+    // var ceilnum = Math.ceil(num);
+
+    // document.write("<h1> Number :" + num +"</h1>");
+    // document.write(" <h1> Round of  Value  :" + roundnum +"</h1>");
+    // document.write(" <h1> Floor of  Value  :" + floornum +"</h1>" );
+    // document.write(" <h1> ceil value :" + ceilnum +"</h1>");
+
+
+
+// Q2. 
+
+// Write a program that takes a nagative integer from user & display the following in your browser.
+//  a. number
+//   b. round off value of the number 
+//   c. floor value of the number
+    // d. ceil value of the number
+
+    
+    // var num =  -prompt("type any number please","Enter here");
+
+    // var roundnum = Math.round(num);
+    // var floornum = Math.floor(num);
+    // var ceilnum = Math.ceil(num);
+
+    // document.write("<h1> Number :" + num +"</h1>");
+    // document.write(" <h1> Round of  Value  :" + roundnum +"</h1>");
+    // document.write(" <h1> Floor of  Value  :" + floornum +"</h1>" );
+    // document.write(" <h1> ceil value :" + ceilnum +"</h1>");
+
+
+
+// Q3. 
+        //  Write a program that displays the absolute value of a number. 
+        // E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+        // var num = prompt("for absolute vlaue","Enter here");
+
+        // var absnum = Math.abs(num);
+        // document.write(" <h1> absolute value of   :" + num +" is :" + absnum+ " </h1>" );
+
+     
+
+
+        
+// Q4. 
+//      Write a program that simulates a dice using random() method of JS Math class. 
+//        Display the value of dice in your browser.:
+
+// var dice = Math.random();
+
+// dice = dice*6  ;
+// dice = Math.ceil(dice);
+
+// document.write(" <h1> Random dice value :" + dice + " </h1>");
+
+// Q5. 
+// Write a program that simulates a coin toss using random() method of JS Math class.
+//  Display the value of coin in your browser
+
+
+// var coin = Math.random();
+
+// coin = coin*2  ;
+// coin = Math.ceil(coin);
+// document.write( "<h1>" + coin + "</h1>" );
+
+// if ( coin === 1){
+//     document.write(" <h1> Random coin value :  Tails </h1>");
+// }
+// else {
+//     document.write(" <h1> Random coin value : Heads </h1>");
+
+
+
+
+    //   Q6. 
+        // Write a program that shows a random number between 1 and 100 in your browser.
+
+
+        // var randNum = Math.random();
+        //     randNum = randNum*100;
+        //     randNum = Math.ceil(randNum);
+
+        //     document.write(" <h1> Random number between 1 and 100 : "+   randNum +"</h1>");
+
+
+
+        // Q7.
+        //  Write a program that asks the user about his weight. 
+        // Parse the user input and display his weight in your browser. Possible user inputs can be:
+
+
+        // var  usreWeight = +prompt("Enter your weight " ,"type here please");
+        //     usreWeight = parseFloat(usreWeight);
+
+        //     document.write(" <h1> The weight of user is : "+   usreWeight +" kilogram </h1>"); 
+        //     // if we type anin string in input then anwser wll comes in " NaN"
+
+
+
+        // // Q8. 
+        // Write a program that stores a random secret number from 1 to 10 in a variable. 
+        //     Ask the user to input a number between 1 and 10. 
+        //     If the user input equals the secret number, congratulate the user.
+
+
+            // var secretNum = 5;
+            // var input = +prompt("Enter number between 1 -  10 ","Enter here");
+
+            // document.write("<h1> your Entered Number is :" + input + "</h1>");
+
+            // if ( input === secretNum){
+            //     document.write("<h1>  Very Good! you entered a absolutely secret number </h1>")
+            // }
+            // else {
+            //     document.write("<h1>  Try again for secret number </h1>")
+            // }
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
